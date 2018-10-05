f49c81cb6156a021f80252bcfdc817614334e546
korzenioc9 <36169270+korzenioc9@users.noreply.github.com>
2018-10-05 18:18:34 -0400

    Update main.c

--- Software PWM/G2553/main.c
+++ Software PWM/G2553/main.c
@@ -2,7 +2,7 @@
 // Date: 10/02/2018
 
 #include <msp430.h> 
-int Duty_Cycle = 500;
+int Duty_Cycle = 500; //sets LED duty cycle to 50% to start
 int debounce_state = 0;
 void LEDSetup();
 void ButtonSetup();
@@ -35,29 +35,29 @@ __interrupt void P1_ISR(void){
             switch(debounce_state)
                 {
                 case 0: //OFF -> GOING ON
-                    TA1CTL = TASSEL_2 + MC_1 + TACLR; // SMCLK (1mhz), continuous mode, clear timer
+                    TA1CTL = TASSEL_2 + MC_1 + TACLR; // SMCLK, continuous mode, clear timer
                     P1IE &= ~BIT3; //disable interrupts for P1.3
                     P1IFG &= ~BIT3; //Clear flag P1.3
                     break;
                 case 1: //ON -> GOING OFF
-                    TA1CTL = TASSEL_2 + MC_1 + TACLR; // SMCLK (1mhz), continuous mode, clear timer
+                    TA1CTL = TASSEL_2 + MC_1 + TACLR; // SMCLK, continuous mode, clear timer
                     P1IE &= ~BIT3; //disable interrupts for P1.3
                     P1IFG &= ~BIT3;  //Clear flag p1.3
                     break;
                 }
 }
-#pragma vector=TIMER1_A0_VECTOR
+#pragma vector=TIMER1_A0_VECTOR //timer A0 interrupt routine
 __interrupt void Timer1_ISR(void){
     switch(debounce_state)
     {
     case 0://turning on, on
-        if(Duty_Cycle < 1000) //Conditions for Incrementing PWM
+        if(Duty_Cycle < 1000) //If Duty cycle is not at 100%
         {
-            Duty_Cycle += 100;
+            Duty_Cycle += 100; //adds 10% brightness
         }
         else
         {
-            Duty_Cycle = 0;
+            Duty_Cycle = 0; //if duty cycle is not less than 1000 (ie @ 100%), reset to 0 (off)
         }
         P1IE |= BIT3; //Enable Interrupts for P1.3
         P1IES &= ~BIT3; //low to high interrupt edge
@@ -82,14 +82,14 @@ void LEDSetup(){
 void ButtonSetup(){
     P1DIR &= ~BIT3;//Set P1.3 to input
     P1REN |= BIT3; //Enable Pull Up Resistor for P1.3 
-    P1OUT |= BIT3; //Active High Button P1.3
+    P1OUT |= BIT3; //Active High P1.3
     P1IE |= BIT3;  //Enable Interrupt P1.3 
     P1IES |= BIT3; //P1.3 Interrupt positive edge trigger
     P1IFG &= ~BIT3; //Clear P1.3 Interrupt Flag
 }
 void TimerA0Setup(){
-    TA0CCR0 = 1000;//Set period for Up mode - for PWM
-    TA0CTL |= TASSEL_2 + MC_1 + TACLR; //Use SMCLK (1 Mhz), Up Mode, Clear Timer Registers - for PWM
+    TA0CCR0 = 1000;//Set period for Up mode - for PWM (1000 = 100%)
+    TA0CTL |= TASSEL_2 + MC_1 + TACLR; //Use SMCLK, Up Mode, Clear Timer Registers - for PWM
 }
 void TimerA1Setup(){
     TA1CCTL0 = CCIE; //interrupt enabled for debouncing
